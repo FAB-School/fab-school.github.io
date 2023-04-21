22a1366f7642b54f9e637b3cbe823f4b207b3c1f
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-04-22 02:43:44 +0400

    Add 11ty

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const i18n = require('eleventy-plugin-i18n')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(i18n, {
+    translations: {
+      signup: {
+        en: 'Sign up',
+        ru: 'Записаться',
+        tr: 'Üye olmak',
+      },
+    },
+  })
+  return {
+    dir: { input: 'src', output: 'dist' },
+    pathPrefix: '/eleventy-base-blog/',
+  }
+}
