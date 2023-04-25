c1c8e54414bcd707051226d100d0edf5e0f1ab5c
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-04-26 00:32:35 +0400

    Update config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const i18n = require('eleventy-plugin-i18n')
+const filters = require('./utils/filters.js')
+const site = require('./src/_data/site.json');
+const localizedCollections = ['post'];
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(i18n, {
     translations: {
@@ -8,6 +11,28 @@ module.exports = function (eleventyConfig) {
         tr: 'Üye olmak',
       },
     },
+  })
+  // Filters
+  Object.keys(filters).forEach((filterName) => {
+    eleventyConfig.addFilter(filterName, filters[filterName])
+  })
+
+  // For each language, create collection of posts with given language
+  site.langs.map((langEntry) => {
+    for (const localizedCollection of localizedCollections) {
+      // Produces collection with the pluralized name + '_' + locale,
+      // E.g.: 'posts_en'
+      eleventyConfig.addCollection(
+        `${localizedCollection}s_${langEntry.id}`,
+        function (collectionApi) {
+          return collectionApi
+            .getFilteredByTag(localizedCollection)
+            .filter(function (item) {
+              return item.data.locale === langEntry.id
+            })
+        }
+      )
+    }
   })
   return {
     dir: { input: 'src', output: 'public' },
